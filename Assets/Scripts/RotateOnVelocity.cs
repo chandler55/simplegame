304b00a6f54fed81b8c0da52664c2eeb1749624a
Michael Le <michaelle@gmail.com>
2014-02-25 00:22:13 -0500

    - rock manager

--- Assets/Scripts/RotateOnVelocity.cs
+++ Assets/Scripts/RotateOnVelocity.cs
@@ -17,11 +17,7 @@ public class RotateOnVelocity : MonoBehaviour
 	
 	void Update () 
 	{
-		Debug.Log ( rigidbody2D.velocity );
-		//m_rotation = rigidbody2D.velocity.y;
-
 		m_rotation = Mathf.SmoothDampAngle( m_rotation, rigidbody2D.velocity.y, ref m_currentVelocity, smoothingTime );
-		//Vector3.SmoothDamp( 
 		transform.rotation = Quaternion.Euler( new Vector3( 0, 0, m_rotation ) );
 	}
 }
